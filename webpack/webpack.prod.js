b3e0ed67a78d24f9f0508f05cb7bcf3a178107e7
Aleksandr Levochkin <aleksandrlevochkin@Aleksandrs-MacBook-Air.local>
2024-05-05 06:16:27 +0200

    Change url

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -23,9 +23,8 @@ module.exports = merge(common, {
         new webpack.DefinePlugin({
             "process.env.REACT_APP_AUTH_ENABLED": JSON.stringify(true),
             "process.env.REACT_APP_USE_CUSTOM_API": JSON.stringify(true),
-            "process.env.REACT_APP_BASE_URL": JSON.stringify(
-                "https://api.myhnfeed.com"
-            ),
+            "process.env.REACT_APP_BASE_URL":
+                JSON.stringify("https://nginx:444"),
             "process.env.REACT_APP_CLIENT_URL": JSON.stringify(
                 "https://www.myhnfeed.com"
             ),
